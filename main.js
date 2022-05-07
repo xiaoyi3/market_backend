213bf3c9e4fdf99f728155dfd17ddca8f5e065bf
bianyuanop <m13320507359@163.com>
2022-05-07 17:54:47 +0800

    prototype

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const config = require('./config');
+const {DataManager} = require('./lib/dataManager');
+const {dbConfig} = require('./config');
+const {generateError} = require('./lib/errors');
 
-const express = require('express')
-const app = express()
+const knexConf = dbConfig[dbConfig.useDB];
+
+const express = require('express');
+const app = express();
+const dbm = new DataManager(knexConf);
+
 
 app.get('/', (req, res) => {
-    res.send(JSON.stringify({
-        message: 'Hello World!'
-    }))
-})
+  res.send(JSON.stringify({
+    message: 'Hello World!',
+  }));
+});
+
+app.get('/asset/:id', async (req, res) => {
+  const id = req.params.id;
+  const asset = await dbm.queryAsset(id);
+  if (asset) {
+    res.json(asset);
+  } else {
+    res.json(generateError('null'));
+  }
+});
 
 app.listen(config.port, () => {
-    console.log(`Server is running on port ${config.port}`)
-})
+  console.log(`Server is running on port ${config.port}`);
+});
